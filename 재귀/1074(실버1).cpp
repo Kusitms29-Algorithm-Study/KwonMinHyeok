628ebf3f568e9f5d4c8e8bad344526d8d9f97f8e
KMH <seaweed8759@naver.com>
2024-03-25 00:59:39 +0900

    4th problem solving - v2 update

--- 재귀/1074(실버1).cpp
+++ 재귀/1074(실버1).cpp
@@ -1,41 +1,35 @@
 #include <iostream>
+#include "cmath"
 
 using namespace std;
 
-int n, r, c;
-int ans;
+int r,c;
 
-void Z(int y, int x, int size)
-{
-    if (y == r && x == c)
-    {
-        cout << ans << '\n';
-        return;
+void z(int n, int row, int col, int startValue){
+    int temp = pow(2,n-1);
+    if(temp*temp<=2){
+        if(r==row && c == col) cout << startValue;
+        else if (r == row && c == col+1) cout << startValue +1;
+        else if (r == row+1 && c == col) cout << startValue +2;
+        else if (r == row+1 && c == col+1) cout << startValue +3;
     }
-
-    // r,c가 현재 사분면에 존재한다면
-    if (r < y + size && r >= y && c < x + size && c >= x)
-    {
-        // 1사분면 탐색
-        Z(y, x, size / 2);
-        // 2사분면 탐색
-        Z(y, x + size / 2, size / 2);
-        // 3사분면 탐색
-        Z(y + size / 2, x, size / 2);
-        // 4사분면 탐색
-        Z(y + size / 2, x + size / 2, size / 2);
-    }
-    else
-    {
-        ans += size * size;
+    else{
+        if(r<row+temp && c<col+temp) z(n-1, row,col,startValue);
+        else if(r<row+temp && c>=col+temp) z(n-1, row, col+temp, startValue+(temp*temp));
+        else if(r>=row+temp && c<col+temp) z(n-1, row+temp, col, startValue+(temp*temp*2));
+        else if(r>=row+temp && c>=col+temp) z(n-1, row+temp, col+temp, startValue+(temp*temp*3));
     }
 }
-int main()
-{
+
+int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-
+    
+    int n;
     cin >> n >> r >> c;
-    Z(0, 0, (1 << n));
+    
+    z(n,0,0,0);
+    
     return 0;
+    
 }
