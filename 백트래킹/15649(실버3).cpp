6b9bfa6712b37824371eed211de74c312c7a4b58
KMH <seaweed8759@naver.com>
2024-03-31 02:15:36 +0900

    5th problem solving

---
+++ 백트래킹/15649(실버3).cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+using namespace std;
+
+int n,m;
+int check[8] ={};
+
+void fun(int* arr, int index){
+    if(index == m){
+        for(int a=0;a<m;a++){
+            cout << arr[a] << " ";
+        }
+        cout << "\n";
+        return ;
+    }
+    
+    for(int i=1;i<=n;i++){
+        if(!check[i-1]){
+            arr[index] = i;
+            check[i-1] =1;
+            fun(arr,index+1);
+            check[i-1] = 0;
+        }
+    }
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    cin >> n >> m;
+    
+    int arr[m];
+    
+    fun(arr,0);
+    
+    return 0;
+}
